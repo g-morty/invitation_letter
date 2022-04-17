8347df0aced98da9dbd15c19eef8429ffaeefeab
gaiwenbin@born4bonor.com <gaiwenbin@born4honor.com>
2022-04-18 01:10:53 +0800

    backup

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import {createRouter,createWebHashHistory,createWebHistory} from 'vue-router'
 
 export default new createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     { path: '/', component: () => import('../pages/index') },
     { path: '/invitation_letter', component: () => import('../pages/invitation_letter/') },
